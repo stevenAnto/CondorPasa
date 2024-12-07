b1a3562392430f02a23eaf639086cd58101e0a7b
stevenAnto <ecalcinap@unsa.edu.pe>
2024-12-07 15:26:33 -0500

    primerNivel

--- Assets/scripts/controlCondor.cs
+++ Assets/scripts/controlCondor.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class controlCondor : MonoBehaviour
 {
     Rigidbody rigidbody;
     Transform transform;
+    private bool isRotationFrozen = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,26 +18,59 @@ public class controlCondor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       Impulso();
-       Rotacion();
-        
+        Impulso();
+        Rotacion();
+
     }
-    private void Impulso(){
-        if(Input.GetKey(KeyCode.Space)){
+    private void Impulso()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
             rigidbody.AddRelativeForce(Vector3.up);
             print("impulso");
+              // Reactivar rotación si estaba congelada
+            if (isRotationFrozen)
+            {
+                rigidbody.freezeRotation = false;
+                isRotationFrozen = false;
+            }
         }
-        if(Input.GetKey(KeyCode.A)){
+
+    }
+    private void Rotacion()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
             transform.Rotate(Vector3.left);
             print("ziquierda");
         }
-        else if(Input.GetKey(KeyCode.D)){
+        else if (Input.GetKey(KeyCode.D))
+        {
             transform.Rotate(Vector3.right);
             print("derecha");
-            
+
         }
     }
-    private void Rotacion(){
-
+    private void OnCollisionEnter(Collision collision){
+        switch (collision.gameObject.tag){
+            case "combustible":
+                print("combustible");
+                 rigidbody.freezeRotation = true;
+                isRotationFrozen = true;
+                break;
+            case "seguro":
+                print("seguro");
+                 rigidbody.freezeRotation = true;
+                isRotationFrozen = true;
+                break;
+            case "siguienteNivel":
+                print("llegaste");
+                SceneManager.LoadScene("nivel2");
+                break;
+            default:
+                SceneManager.LoadScene("nivel1");
+                print("muerto");
+                break;
+        }
     }
 }
